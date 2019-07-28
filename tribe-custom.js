8114db42e5b288975c2a33216249e4dc17166154
rafsuntaskin <rafsun.aiub@gmail.com>
2019-07-28 12:05:47 +0600

    updated JS to improve performance on large DOM

--- tribe-custom.js
+++ tribe-custom.js
@@ -1,13 +1,19 @@
-jQuery(document).ready( function ($) {
-    // RSVP, Woo, and EDD tickets default to quantity of 1
-    $( 'input.tribe-ticket-quantity, .woocommerce .quantity input.qty, .edd.quantity input.edd-input' ).val( 1 );
+;(function ($) {
+    var wrapper = $('.tribe-events-tickets');
 
-    //RSVP Trigger
-    $('input.tribe-ticket-quantity, .quantity input, .quantity select').trigger('change');
+    var Tribe_Ticket_Qty = {
+        init: function () {
+            var self = this;
+            // wrapper.find( 'input.tribe-ticket-quantity, .woocommerce .quantity input.qty, .edd.quantity input.edd-input' ).val(1);
+            $inputs = wrapper.find('input.tribe-ticket-quantity');
+            $inputs.val(1);
+            $inputs.trigger('change');
+        },
 
-    //TPP and Ticket form trigger
-    $tickets_lists = $( '.tribe-events-tickets, .tribe-events-tickets-tpp' );
-    $quantity_fields = $tickets_lists.find( '.quantity' ).find( '.qty, .edd-input' );
+    };
 
-    $quantity_fields.trigger('change');
-});
+    $(function () {
+        Tribe_Ticket_Qty.init();
+    });
+
+})(jQuery);
